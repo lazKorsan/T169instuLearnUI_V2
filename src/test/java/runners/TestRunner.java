73d08e3deb8a497b3b2298025a1cb905341e1ee1
lazKorsan <ahmeteldes2002@gmail.com>
2026-09-26 02:02:00 +0300

    runners

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -19,7 +19,7 @@ import org.junit.runner.RunWith;
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
 		},
 		monochrome = false,
-		tags="@alel",
+		tags="@US005TC02Register",
 		publish=true,
         dryRun = false
 )
